4250e4d4630dae0813cb2df8cedd4bc693a3e84a
Rowan Steger <rowsteg@gmail.com>
2018-07-15 11:24:37 -0600

    menu demo, doesnt deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {MatMenuModule, MatButtonModule, MatIconModule, MatCardModule} from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -16,8 +18,12 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    MatMenuModule, 
+    MatButtonModule, 
+    MatIconModule, 
+    MatCardModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
