745f53153041a06cd80df1fd850e353381418d7c
Corbin <corbinamman@gmail.com>
2018-07-22 17:58:48 -0500

    cleanup unused files and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {MatMenuModule, MatButtonModule, MatIconModule, MatCardModule} from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import {  MatButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { OutdoorsComponent } from './outdoors/outdoors.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    OutdoorsComponent,
     NavbarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatMenuModule, 
-    MatButtonModule, 
-    MatIconModule, 
-    MatCardModule
-    ],
+    BrowserModule, 
+    MatButtonModule 
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
